a8eb72cb5c24c3cb9bc6eafa2011502c560188a3
Andrei Makhnach <ahdpyxa88@gmail.com>
2023-12-17 20:16:12 +0300

    feat: add solutions

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error(`'arr' parameter must be an instance of the Array!`);
+  const controlSequences = new Array (
+        discardNext = '--discard-next', //excludes the next element of the array from the transformed array.
+        discardPrev = '--discard-prev', //excludes the previous element of the array from the transformed array.
+        doubleNext = '--double-next', //duplicates the next element of the array in the transformed array.
+        doublePrev = '--double-prev',//duplicates the previous element of the array in the transformed array.
+  )
+  const res = [];
+  let isDeleted = false;
+
+  arr.forEach((item, index) => {
+    switch (item) {
+      case discardNext:
+        if (index + 1 <= arr.length && !controlSequences.includes(arr[index +1])){
+          res.push(null);
+          isDeleted = true
+        }
+        break;
+      case discardPrev:
+        if (index - 1 >= 0) {
+          res.pop();
+          res.push(null);
+        }
+        break;
+      case doubleNext:
+        if (index + 1 <= arr.length && !controlSequences.includes(arr[index +1]));
+          res.push(arr[index + 1]);
+        break;
+      case doublePrev:
+        if (index - 1 >= 0 && (res.at(-1) != null)) {
+          res.push(arr[index - 1])
+        }
+        break;
+      default:
+        if (isDeleted) {
+          res.push(null);
+          isDeleted = false;
+        }else{
+          res.push(item);
+        }
+        break;
+    }
+  })
+  return res.filter(item => item != null);
+
 }
 
 module.exports = {
